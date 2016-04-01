407fd66c8a81a06a72867cf0a8e3f9051086d907
Spencer Uresk <Spencer.Uresk@experticity.com>
2016-04-01 17:28:20 -0600

    Making NPM module

--- index.js
+++ index.js
@@ -1,16 +1,33 @@
-var blinks = document.getElementsByTagName("blink");
-for(i = 0; i < blinks.length; i++) {
-  (function(i){
-    var blink = blinks[i];
-    var interval = blink.getAttribute("data-interval") || 500;
-    var displayStyle = window.getComputedStyle(blink, null).getPropertyValue("display") || blink.style.display || 'block';
-    setInterval(function(){
-      if(blink.style.display && blink.style.display == displayStyle) {
-        blink.style.display = 'none';
+(function (root, factory) {
+    if (typeof define === 'function' && define.amd) {
+        // AMD. Register as an anonymous module.
+        define([], factory);
+    } else if (typeof module === 'object' && module.exports) {
+        // Node. Does not work with strict CommonJS, but
+        // only CommonJS-like environments that support module.exports,
+        // like Node.
+        module.exports = factory();
+    } else {
+        // Browser globals (root is window)
+        root.returnExports = factory();
+  }
+}(this, function () {
+    return function(){
+      var blinks = document.getElementsByTagName("blink");
+      for(i = 0; i < blinks.length; i++) {
+        (function(i){
+          var blink = blinks[i];
+          var interval = blink.getAttribute("data-interval") || 500;
+          var displayStyle = window.getComputedStyle(blink, null).getPropertyValue("display") || blink.style.display || 'block';
+          setInterval(function(){
+            if(blink.style.display && blink.style.display == displayStyle) {
+              blink.style.display = 'none';
+            }
+            else {
+              blink.style.display = displayStyle;
+            }
+          }, interval);
+        }(i));
       }
-      else {
-        blink.style.display = displayStyle;
-      }
-    }, interval);
-  }(i));
-}
+    };
+}));
